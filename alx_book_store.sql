19dfda1c9b8e6b25fa514cfc3dfa1bf6878c844f
Kam Ephrem <kamnoah88@gmail.com>
2024-07-27 23:23:30 +0100

    edited alx_book_store.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,15 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-CREATE TABLE Books(book_id VARCHAR(50) PRIMARY KEY, title VARCHAR(130), author_id INT FOREIGN KEY, price DOUBLE, publication_date DATE)
+CREATE TABLE Books(
+    book_id VARCHAR(50) PRIMARY KEY, 
+    title VARCHAR(130), 
+    author_id INT, 
+    price DOUBLE, 
+    publication_date DATE
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
