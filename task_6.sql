10da8f125cc53bfec59c5824fda131908231e688
Kam Ephrem <kamnoah88@gmail.com>
2024-07-28 16:56:31 +0100

    added task_6.sql file

---
+++ task_6.sql
@@ -0,0 +1,24 @@
+INSERT INTO customers(
+     customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+VALUES (
+    2, 
+    'Cole Baidoo', 
+    'cbaidoo@sandtech.com', 
+    '124 Happiness Ave.'
+)
+VALUES(
+    3,
+    'Obed Ehoneah',
+    'eobed@sandtech.com',
+    '125 Happiness Ave.'
+)
+VALUES(
+    4,
+    'Nehemial Kampo',
+    'nkamolu@sandthech.com',
+    '126 Happiness Ave.'
+)
