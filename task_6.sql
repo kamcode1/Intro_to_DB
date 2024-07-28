e5b2094303df66d30af68afc3af98ca0d54e7965
Kam Ephrem <kamnoah88@gmail.com>
2024-07-28 17:09:11 +0100

    Edited task_6.sql file

--- task_6.sql
+++ task_6.sql
@@ -8,17 +8,17 @@ VALUES (
     2, 
     'Blessing Malik', 
     'bmalik@sandtech.com', 
-    "124 Happiness  Ave."
+    '124 Happiness  Ave.'
 ),
 (
     3,
     'Obed Ehoneah',
     'eobed@sandtech.com',
-    "125 Happiness Ave."
+    '125 Happiness  Ave.'
 ),
 (
     4,
     'Nehemial Kampo',
     'nkamolu@sandthech.com',
-    "126 Happiness Ave."
+    '126 Happiness  Ave.'
 );
