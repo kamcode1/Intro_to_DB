19fa947b7595e579d81af1b6847d566956e3bdef
Kam Ephrem <kamnoah88@gmail.com>
2024-07-28 16:43:56 +0100

    added task_5.sql file

---
+++ task_5.sql
@@ -0,0 +1,12 @@
+INSERT INTO Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+VALUES (
+    1, 
+    'Cole Baidoo', 
+    'cbaidoo@sandtech.com', 
+    '123 Happiness Ave.'
+);
